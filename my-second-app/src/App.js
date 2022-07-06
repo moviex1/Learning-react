6f3d69ff5504ae5283c5b2bf01c53695ab752252
Nikita <moviex@inbox.ru>
2022-07-06 17:32:33 +0300

    Added Routes

--- my-second-app/src/App.js
+++ my-second-app/src/App.js
@@ -3,17 +3,22 @@ import Profile from "./components/Profile/Profile";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Dialogs from "./components/Dialogs/Dialogs";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Nav />
-      <div className='app-wrapper-content'>
-       <Profile /> 
-      <Dialogs />
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Nav />
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/dialogs" element={<Dialogs />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
